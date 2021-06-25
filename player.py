54caade98c8ca1e70c6d88ae8fee2922f1421ea6
seyit <seyit@synopsys.com>
2021-06-25 15:32:12 +0200

    fixes

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ from montecarlo import *
 import signal, time
 from signal import *
 
-
+total_time = 0
 
 class Player:
     VERSION = "Royal Flush v4"
@@ -13,21 +13,27 @@ class Player:
     def betRequest(self, game_state):
         idx = game_state["in_action"]
         stack = game_state["players"][idx]["stack"]
+        current_bet = game_state["players"][idx]["bet"]
         bet = 0
-        min_bet = game_state["current_buy_in"] - game_state["players"][idx]["bet"]
+        min_bet = game_state["current_buy_in"] - current_bet
         raise_amount = min_bet * 2
-        max_bet = 300
+        max_bet = 400
+
+        time_start = time.time()
 
         time_per_round = 100
-        if game_state["round"] > 10000 / time_per_round:
-            if min_bet > max_bet:
+        global total_time
+        if game_state["round"] == 0:
+            total_time = 0
+
+        if total_time > 15000:
+            if min_bet + current_bet > max_bet:
                 return 0
             bet = min_bet
             if bet > stack:
                 return stack
             return bet
 
-        #if True:
         try:    
             """
             cards = game_state["community_cards"]
@@ -52,11 +58,11 @@ class Player:
                 # second round
                 if len(game_state["community_cards"]) == 4:
                     min_chance = 0.20
-                    raise_amount = raise_amount * 3
+                    raise_amount = raise_amount * 2
                 # third round
                 if len(game_state["community_cards"]) == 5:
                     min_chance = 0.30
-                    raise_amount = raise_amount * 6
+                    raise_amount = raise_amount * 5
                 if win > 2 * min_chance:
                     bet = min_bet + raise_amount
                 elif win > min_chance:
@@ -64,7 +70,7 @@ class Player:
                 else:
                     bet = 0
 
-            if min_bet > max_bet:
+            if min_bet + current_bet > max_bet:
                 return 0
 
             if win <= 0:
@@ -73,6 +79,9 @@ class Player:
             bet = min_bet
         if bet > stack:
             return stack
+        
+        total_time = (time.time() - time_start) * 1000
+
         return bet
 
     def showdown(self, game_state):
