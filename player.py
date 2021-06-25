e64a329f02f6658e2971055d1d70cac9995a7944
seyit <seyit@synopsys.com>
2021-06-25 14:43:06 +0200

    v3 bot

--- player.py
+++ player.py
@@ -2,9 +2,13 @@
 from card import *
 from hand import *
 from montecarlo import *
+import signal, time
+from signal import *
+
+
 
 class Player:
-    VERSION = "Royal Flush v2"
+    VERSION = "Royal Flush v3"
 
     def betRequest(self, game_state):
         idx = game_state["in_action"]
@@ -12,8 +16,18 @@ class Player:
         bet = 0
         min_bet = game_state["current_buy_in"] - game_state["players"][idx]["bet"]
         raise_amount = min_bet * 2
+        
+        time_per_round = 100
+        if game_state["round"] > 20000 / time_per_round:
+            print("TIMEOUT!")
+            bet = min_bet
+            if bet > stack:
+                return stack
+            return bet
 
-        try:
+        #if True:
+        try:    
+            """
             cards = game_state["community_cards"]
             cards.append(game_state["players"][idx]["hole_cards"][0])
             cards.append(game_state["players"][idx]["hole_cards"][1])
@@ -22,11 +36,10 @@ class Player:
                 bet = min_bet * 10 
             elif min_bet > 100:
                 return 0
-            win = 0
             """
-            pocket = Hand(Card.getCards(game_state["players"][idx]["hole_cards"]))
-            community = Hand(Card.getCards(game_state["community_cards"]))
-            #win = MonteCarlo.run(pocket, community, 5, 1000)[0]
+            pocket = Card.getCards(game_state["players"][idx]["hole_cards"])
+            community = Card.getCards(game_state["community_cards"])
+            win = MonteCarlo.run(pocket, community, 5, time_per_round)[0]
             print(win)
             # zero round
             if len(game_state["community_cards"]) == 0:
@@ -46,11 +59,12 @@ class Player:
                 bet = min_bet
             else:
                 bet = 0
-            """
+
             if win <= 0:
                 bet = min_bet
         except :
             bet = min_bet
+            print("EXCEPTION!")
         if bet > stack:
             return stack
         return bet
