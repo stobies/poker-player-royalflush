5cbd816fe6bb2b25855bf44e92117cc469551b7d
Paul Filip <pfilip@synopsys.com>
2021-06-25 11:44:36 +0300

    parser for cards

--- card.py
+++ card.py
@@ -3,10 +3,45 @@ class Card:
     numbers = list(range(2, 15))
     suits = ['H','S','C','D']
 
+    def __init__(self):
+        self.number = 0
+        self.suit = 'X'
+
     def __init__(self, number, suit):
         self.number = number
         self.suit = suit
 
     def getCard(self):
         return [self.number, self.suit]
+
+    def setCard(self, number, suit):
+        self.number = number
+        self.suit = suit
+
+    def getCards(cardsList):
+        intRanks = ["2", "3", "4", "5", "6", "7", "8", "9", "10"]
+        charRanks = ["J", "Q", "K", "A"]
+        apiSuits = ["hearts", "spades", "clubs", "diamonds"]
+        cards = []
+        for card in cardsList:
+            newCard = Card()
+            rank = card["rank"]
+            suit = card["suit"]
+            cardNumber = 0
+            if rank in intRanks:
+                cardNumber = intRanks.index(rank) + 2
+            
+            if rank in charRanks:
+                cardNumber = charRanks.index(rank) + 11
+            
+            suitIndex = apiSuits.index(suit)
+            newCard.setCard(cardNumber, newCard.suits[suitIndex])
+            cards.append(newCard)
+        
+        return cards
+
+            
+
+
+
     
