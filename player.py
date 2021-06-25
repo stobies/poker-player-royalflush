fe3d4632ebe7ed13b9cd458c92569747c53e2ab1
seyit <seyit@synopsys.com>
2021-06-25 10:09:12 +0200

    first commit

--- player.py
+++ player.py
@@ -1,9 +1,14 @@
 
 class Player:
-    VERSION = "Default Python folding player"
+    VERSION = "Royal Flush v1"
 
     def betRequest(self, game_state):
-        return 0
+        idx = game_state["in_action"]
+        bet = game_state["current_buy_in"] - game_state["players"][idx]["bet"]
+        stack = game_state["players"][idx]["stack"]
+        if bet > stack:
+            return stack
+        return bet
 
     def showdown(self, game_state):
         pass
