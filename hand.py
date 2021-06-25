e4d138d9117a356970163f1e69f612f0fd7bfcdb
Stephan Tobies <stephan@tobies.info>
2021-06-25 10:55:32 +0200

    fixed flush detection algorithm

--- hand.py
+++ hand.py
@@ -1,5 +1,19 @@
 
+from enum import Enum
+
 from card import Card
+
+
+class Value(Enum):
+    HIGH_CARD = 1
+    PAIR = 2
+    TWO_PAIRS = 3
+    THREE = 4
+    STRAIGHT = 5
+    FLUSH = 6
+    FULL_HOUSE = 7
+    FOUR = 8
+    STRAIGHT_FLUSH = 9
 
 class Hand:
 
@@ -25,11 +39,11 @@ class Hand:
         for c in cards:
             suit_count[c.suit] += 1
 
-        for (suit,count) in suit_count:
+        for (suit,count) in suit_count.items():
             if count == 5:
-                return suit
+                return True
 
-        return ''
+        return False
 
     def get_straight(cards):
         
@@ -45,3 +59,29 @@ class Hand:
                 return False
 
         return count == 5
+
+    # def __init__(self, cards):
+    #     cards = sorted(cards, lambda card: card.number)
+
+    #     flush = Hand.get_flush(cards)
+    #     straight = Hand.get_straight(cards)
+
+    #     if flush and straight:
+    #         self.value = Value.STRAIGHT_FLUSH
+    #     elif straight:
+    #         self.value = Value.STRAIGHT
+        
+    #     quad = Hand.get_runs(cards, 4)
+    #     if quad:
+    #         self.value = Value.FOUR
+    #         self.card = quad[0]
+
+    #     triples = Hand.get_runs(cards, 3)
+    #     pairs = Hand.get_runs(cards, 2)
+
+    #     if triples and pairs:
+    #         self.value = Value.FULL_HOUSE
+
+
+
+
